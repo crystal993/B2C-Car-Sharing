258088789a226fbc1d4613f00b79b91e031b9285
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-11-02 12:08:16 +0900

    ⭐ feat : GlobalStyle, Theme, font 설정 - GlobalStyle : styled-reset 적용, 폰트 적용 등 - Theme : 반응형 크기, 색상 설정

---
+++ src/styles/Theme.jsx
@@ -0,0 +1,16 @@
+const size = {
+  mobile_s: '360px',
+  mobile_l: '450px',
+};
+
+const color = {
+  mainColor: '#0094FF',
+  gray: '#D9D9D9',
+  black: '#000000',
+  bgColor: '#FFFFFF',
+};
+
+export const Theme = {
+  size,
+  color,
+};
