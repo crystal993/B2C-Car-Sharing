c05ad8e64f0433fce5a8c41eca3b756081291e85
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-11-03 15:37:48 +0900

    ⭐ feat : NotFound, SwiperTags 컴포넌트 생성 - NotFound : url 경로를 잘못 입력했을 때 반환되는 페이지 - SwiperTags : 태그들을 스와이프 하는 것이 가능하다.

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return <Wrapper>ERROR || 잘못된 경로 입니다.</Wrapper>;
 };
+
+const Wrapper = styled.div`
+  height: 100vh;
+  text-align: center;
+  font-weight: 700;
+  font-size: 1.7rem;
+  line-height: 2.1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default NotFound;
